cbc79a92f1a6f651702a1c69e2aae8b7827fe1b2
Shingo Matsushima <s.matsushima@sen-do.com>
2018-01-02 14:16:53 +0000

    ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # helperメソッド使用の為のインクルード
+  include SessionsHelper
+
+  private
+
+
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
 end
