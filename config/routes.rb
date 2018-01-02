e36c94eb8004e2f67383018e50351f80eb8fe9be
Shingo Matsushima <s.matsushima@sen-do.com>
2018-01-02 14:04:12 +0000

    ユーザ管理機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   # TOPページ
   root to: 'toppages#index'
 
+  # ログイン(一旦、変更・削除はなし)
+  get 'signup', to: 'users#new'
+  resources :users, only: [:show, :new, :create]
+  
+  # セッション管理
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+
 end
